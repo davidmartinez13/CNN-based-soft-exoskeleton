cfe994bcbd709b2ec485900c7e9943d7068102d3
davidmartinez13 <dsmartinez13@hotmail.com>
2023-07-14 16:20:19 +0200

    added control via rosrun

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/david/exo_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/david/soft_exo_ws/devel;/opt/ros/noetic'.split(';') if '/home/david/soft_exo_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/david/exo_ws/devel;/home/david/soft_exo_ws/devel;/opt/ros/noetic'.split(';') if '/home/david/exo_ws/devel;/home/david/soft_exo_ws/devel;/opt/ros/noetic' != '' else []
--- src/servo_control/scripts/publisher.py
+++ src/servo_control/scripts/publisher.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String, UInt16, Float32
+from std_msgs.msg import String, UInt16, Float32, Float32MultiArray
+
+def deg2rad(degree):
+    return degree * 3.14159265359/180
+
+def rad2deg(radian):
+    return radian * (180 / 3.14159265359)   
 
 def talker():
-    pub = rospy.Publisher('servo', Float32, queue_size=10)
+    pub = rospy.Publisher('servo', Float32MultiArray, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    q_array = Float32MultiArray()
+    delta_t = 5
+    deg = float(input('enter staring angle'))
+    q1 = deg
+    q2 = deg
     while not rospy.is_shutdown():
         try:
-            data = float(input())
-            if 120.0 >= data>= 30.0:
-            
-            # if data:    
-                rospy.loginfo(data)
-                pub.publish(data)
-                rate.sleep()
+            q1 = delta_t + q1
+            q2 = delta_t + q2
+            q_array.data.clear()
+            q_array.data.append(q1)
+            q_array.data.append(q2)
+            rospy.loginfo(q_array.data)
+            pub.publish(q_array)
+            rate.sleep()
+            if q1>=180 or q2>=180:
+                break
         except:
-            print('Data not float')
+            print('Failed to send angle')
 if __name__ == '__main__':
     try:
         talker()
